773a39a26527b3586a9ae0f95f8e99e1622d4051
razvan841 <rstefan841@gmail.com>
2023-05-24 11:45:58 +0200

    CreateContract xmlRootElement

---
+++ src/main/java/nl/earnit/models/resource/contracts/CreateContract.java
@@ -0,0 +1,31 @@
+package nl.earnit.models.resource.contracts;
+
+import jakarta.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement
+public class CreateContract {
+
+    private String role;
+    private String description;
+
+    public CreateContract(String role, String description) {
+        this.role = role;
+        this.description = description;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
