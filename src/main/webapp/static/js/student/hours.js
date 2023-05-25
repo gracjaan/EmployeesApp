e3a8688bf01024cfa67c69e9fa8bc10bd6e4c10d
gracjaan <gchmielnicki3005@gmail.com>
2023-05-25 10:57:15 +0200

    Adding edit entry feature

--- src/main/webapp/static/js/student/hours.js
+++ src/main/webapp/static/js/student/hours.js
@@ -60,6 +60,25 @@ function submitForm (uid, ucid, year, week, date, hours, position, description) 
         .catch(e => console.error(e))
 }
 
+function submitEdittedForm (uid, ucid, year, week, date, hours, position, description) {
+
+    if (validateForm() == false){
+        return;
+    }
+
+    let json = {date: date, hours: hours, position: position, description: description}
+    fetch ("/users/"+ uid + "/contracts/" + ucid + "/worked/" + year + "/" + week,
+        {
+            method: "PUT",
+            body: JSON.stringify(json),
+            headers: {
+                "Content-type" : "application/json",
+                "Accept" : "application/json"
+            }
+        })
+        .catch(e => console.error(e))
+}
+
 function validateForm () {
     const dateInput = document.getElementById('date').value;
     const hoursInput = document.getElementById('hours').value;
@@ -72,4 +91,33 @@ function validateForm () {
     }
 
     return true;
+}
+
+
+function toggleEdit(button) {
+    const entry = button.parentNode;
+    const textElements = entry.querySelectorAll('.text-text');
+    const editButton = entry.querySelector('.edit-button');
+
+    textElements.forEach(element => {
+        const isEditable = element.contentEditable === 'true';
+        element.contentEditable = !isEditable;
+    });
+
+    const isEditing = entry.classList.contains('editing');
+    entry.classList.toggle('editing', !isEditing);
+    editButton.innerHTML = isEditing ? '<img src="/earnit/static/icons/pencil.svg" class="h-6 w-6" alt="pencil" />' : '<img src="/earnit/static/icons/checkmark.svg" class="h-6 w-6" alt="arrow" />';
+
+    if (!isEditing) {
+        // Submission logic here
+        const updatedData = {
+            date: textElements[0].textContent,
+            hours: textElements[1].textContent,
+            position: textElements[2].textContent,
+            description: textElements[3].textContent
+        };
+
+        // Send the updatedData to the server or perform any necessary actions
+        submitEdittedForm();
+    }
 }
