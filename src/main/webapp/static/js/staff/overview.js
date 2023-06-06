699403c2bfcf3c3fc666d59764725c6c8a831402
razvan841 <rstefan841@gmail.com>
2023-06-06 18:08:30 +0200

    Finished JS for overview page

--- src/main/webapp/static/js/staff/overview.js
+++ src/main/webapp/static/js/staff/overview.js
@@ -10,8 +10,15 @@ window.addEventListener("helpersLoaded", async () => {
     const usersElement = document.getElementById("users");
     usersElement.innerText = "";
 
+    const companiesElement = document.getElementById("companies");
+    companiesElement.innerText = "";
+
     for (const user of users) {
         usersElement.append(createUser(user));
+    }
+
+    for (const company of companies) {
+        companiesElement.append(createCompany(company));
     }
 });
 
@@ -29,6 +36,25 @@ function createUser(user) {
     const name = document.createElement("p");
     name.classList.add("text-text");
     name.innerText = getName(user.firstName, user.lastName, user.lastNamePrefix);
+    itemContainer.append(name);
+
+    return li;
+}
+
+function createCompany(company) {
+    const li = document.createElement("li");
+
+    const a = document.createElement("a");
+
+    li.append(a);
+
+    const itemContainer = document.createElement("div");
+    itemContainer.classList.add("bg-primary", "rounded-xl", "w-full", "h-fit", "p-2", "pl-4", "my-2");
+    a.append(itemContainer);
+
+    const name = document.createElement("p");
+    name.classList.add("text-text");
+    name.innerText = company.name;
     itemContainer.append(name);
 
     return li;
