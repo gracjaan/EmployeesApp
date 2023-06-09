c7e02ac70270efd08912daf79286e5e57a7a4fbf
Thomas B <t.ferb1@gmail.com>
2023-06-09 18:57:53 +0200

    Show no invoices

--- src/main/webapp/static/js/company/invoices.js
+++ src/main/webapp/static/js/company/invoices.js
@@ -44,6 +44,13 @@ function updatePage(request) {
     for (const workedWeek of request) {
         entries.appendChild(createEntry(workedWeek));
     }
+
+    if (request === null || request.length < 1) {
+        const noInvoices = document.createElement("div");
+        noInvoices.classList.add("text-text", "font-bold", "w-full", "flex","justify-center","my-2");
+        noInvoices.innerText = "No invoices";
+        entries.append(noInvoices)
+    }
 }
 
 function createEntry(workedWeek) {
