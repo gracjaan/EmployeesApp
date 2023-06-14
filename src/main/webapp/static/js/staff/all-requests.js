a99e8b32907c31c04a9eb298fc8d16616e842703
Kappa-D <dirckmulder20@gmail.com>
2023-06-14 11:48:31 +0200

    made the all-requests page work

--- src/main/webapp/static/js/staff/all-requests.js
+++ src/main/webapp/static/js/staff/all-requests.js
@@ -28,37 +28,40 @@ async function getRejectedWeeks() {
 }
 
 function createRejectedWeek(rejectedWeek) {
+    console.log(rejectedWeek);
     const listElement = document.createElement("li");
     const linkElement = document.createElement("a");
     linkElement.href = "/earnit/staff/reject/request?worked_week=" + rejectedWeek.id;
     const weekDiv = document.createElement("div");
     weekDiv.setAttribute("data-selected", '0')
-    weekDiv.classList.add("justify-between", "flex flex-row", "data-[selected='1']:border-white", "hover:border-text", "border-2", "border-primary", "block bg-primary", "rounded-xl", "w-full", "p-2", "pl-2", "overflow-y-auto", "scrollbar-custom", "scrollbar-track-text", "scrollbar-rounded-xl", "scrollbar-thumb-background", "max-h-[5rem]");
+    weekDiv.classList.add("justify-between", "flex", "flex-row", "data-[selected='1']:border-white", "hover:border-text", "border-2", "border-primary", "block", "bg-primary", "rounded-xl", "w-full", "p-2", "pl-2", "overflow-y-auto", "scrollbar-custom", "scrollbar-track-text", "scrollbar-rounded-xl", "scrollbar-thumb-background", "max-h-[5rem]");
     const employee = document.createElement("p")
     employee.classList.add("text-text");
-    employee.innerText = rejectedWeek.role;
+    employee.innerText = getName(rejectedWeek.user.firstName, rejectedWeek.user.lastName, rejectedWeek.user.lastNamePrefix);
+    console.log(getName(rejectedWeek.user.firstName, rejectedWeek.user.lastName, rejectedWeek.user.lastNamePrefix));
     const company = document.createElement("p")
     company.classList.add("text-text");
-    company.innerText = rejectedWeek.role;
-    const role = document.createElement("p")
-    role.classList.add("text-text");
-    role.innerText = rejectedWeek.role;
+    company.innerText = rejectedWeek.company.name;
     const week = document.createElement("p")
     week.classList.add("text-text");
-    week.innerText = rejectedWeek.role;
+    week.innerText = "WEEK " + rejectedWeek.week;
     const hours = document.createElement("p")
     hours.classList.add("text-text");
-    hours.innerText = rejectedWeek.role;
+    hours.innerText = ((rejectedWeek.totalMinutes) / 60).toString() + " HOURS";
+    console.log(rejectedWeek.totalMinutes);
 
+    weekDiv.append(employee);
+    weekDiv.append(company);
+    weekDiv.append(week);
+    weekDiv.append(hours);
+    linkElement.append(weekDiv);
+    listElement.append(linkElement);
 
-
-
-
-
-function getNotes() {
-
+    return listElement;
 }
 
-function submitForm() {
 
-}
+
+
+
+
