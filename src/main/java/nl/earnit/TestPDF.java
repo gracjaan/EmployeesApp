522c27f0fa199e354e07d9b72fb898e314112277
Thomas B <t.ferb1@gmail.com>
2023-06-19 09:07:35 +0200

    Added test file for creating invoices

---
+++ src/main/java/nl/earnit/TestPDF.java
@@ -0,0 +1,13 @@
+package nl.earnit;
+
+public class TestPDF {
+    public static void main(String[] args) throws Exception {
+        String path = "C:/Users/thoma/Documents/TCS/module 4/pdfs";
+
+// Open document
+//        Document pdfDocument = new Document(path + "/Simplified Invoice.pdf");
+
+//        pdfDocument.save(path + "/invoice.pdf");
+
+    }
+}
