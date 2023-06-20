b346e2a6554fd0723be78235fb1bb95a6c31bd28
Kappa-D <dirckmulder20@gmail.com>
2023-06-20 10:02:50 +0200

    Changes by rstef

--- src/main/webapp/static/js/staff/index.js
+++ src/main/webapp/static/js/staff/index.js
@@ -15,3 +15,63 @@ window.addEventListener("helpersLoaded", async () => {
             name.innerText = "Welcome back, " + json.firstName;
         })
 });
+
+
+function updateChart(workedWeeks) {
+    const labels = [];
+    const ids = [];
+    const dataset = [];
+
+    for (const workedWeek of workedWeeks) {
+        const label = escapeHtml(workedWeek.user.firstName);
+        const hours = workedWeek.totalMinutes / 60
+        const id = workedWeek.user.id;
+
+        if (ids.includes(id)) {
+            dataset[ids.indexOf(id)] += hours;
+        } else {
+            ids.push(id);
+            labels.push(label);
+            dataset.push(hours);
+        }
+    }
+
+    const ctx = document.getElementById('myChart');
+
+    new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: labels,
+            datasets: [{
+                label: 'Hours worked this week per student',
+                data: dataset,
+                backgroundColor: ['rgb(237,76,76)'],
+                color: "white",
+                borderColor: 'rgb(75, 192, 192)',
+                tension: 0.3
+            }]
+        },
+        options: {
+            plugins: {
+                legend: {
+                    labels: {
+                        color: "white"
+                    }
+                }
+            },
+            scales: {
+                y: {
+                    beginAtZero: true,
+                    ticks: {
+                        color: "white"
+                    }
+                },
+                x: {
+                    ticks: {
+                        color: "white"
+                    }
+                }
+            }
+        }
+    });
+}
