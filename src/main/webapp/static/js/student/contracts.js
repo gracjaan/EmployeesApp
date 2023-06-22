92b8e72a13df28c21fc6038610fd5167ccad3b45
gracjaan <gchmielnicki3005@gmail.com>
2023-06-22 11:35:34 +0200

    Adding download single invoice and download all invoices

--- src/main/webapp/static/js/student/contracts.js
+++ src/main/webapp/static/js/student/contracts.js
@@ -76,18 +76,36 @@ function createEntry(contract, invoice) {
     entryContainer.classList.add("rounded-xl", "bg-secondary", "p-4", "relative", "flex-col", "gap-2", "justify-between", "w-full", "h-full", "hidden");
     entryContainer.style.overflowY = "auto";
 
+    const rowcontainer = document.createElement("div");
+    rowcontainer.classList.add("w-full", "flex", "flex-row", "justify-between", "items-center")
+
+    const leftrow = document.createElement("div");
+
     const entryInfo0 = document.createElement("div");
     entryInfo0.classList.add("text-text", "font-bold", "uppercase");
     entryInfo0.innerText = invoice[0].company.name;
-    entryContainer.appendChild(entryInfo0);
+    leftrow.appendChild(entryInfo0);
 
     const entryInfo1 = document.createElement("div");
     entryInfo1.classList.add("text-text", "font-bold", "uppercase");
     entryInfo1.innerText = contract.contract.role;
-    entryContainer.appendChild(entryInfo1);
+    leftrow.appendChild(entryInfo1);
+
+    const rightrow = document.createElement("button");
+    ea.addEventListener("click", () => generateAllInvoices(contract));
+
+    const img = document.createElement("img");
+    img.alt = "download-all"
+    img.classList.add("h-8", "w-8");
+    img.src = "/earnit/static/icons/download-single.svg"
+    rightrow.appendChild(img)
+
+    rowcontainer.appendChild(leftrow);
+    rowcontainer.appendChild(rightrow)
+    entryContainer.appendChild(rowcontainer);
 
     const entryInfo2 = document.createElement("div");
-    entryInfo2.classList.add("text-text", "mt-4", "mb-4");
+    entryInfo2.classList.add("text-text", "mt-4", "mb-4", "text-justify");
     entryInfo2.innerText = contract.contract.description;
     entryContainer.appendChild(entryInfo2);
 
@@ -128,7 +146,33 @@ function createEntry(contract, invoice) {
 }
 
 function generateInvoice (contract, invoice) {
-    return
+    fetch(`/earnit/api/users/${getUserId()}/contracts/${contract.id}/invoices/download/${invoice.year}/${invoice.week}`, {
+        headers: {
+            'authorization': `token ${getJWTCookie()}`,
+        }
+    })
+        .then(async res =>  ({ data: await res.blob(), filename: res.headers.get("content-disposition").split('filename = ')[1] }))
+        .then(({ data, filename }) => {
+            const a = document.createElement("a");
+            a.href = window.URL.createObjectURL(data);
+            a.download = filename;
+            a.click();
+        });
+}
+
+function generateAllInvoices (contract) {
+    fetch(`/earnit/api/users/${getUserId()}/contracts/${contract.id}/invoices/download`, {
+        headers: {
+            'authorization': `token ${getJWTCookie()}`,
+        }
+    })
+        .then(async res =>  ({ data: await res.blob(), filename: res.headers.get("content-disposition").split('filename = ')[1] }))
+        .then(({ data, filename }) => {
+            const a = document.createElement("a");
+            a.href = window.URL.createObjectURL(data);
+            a.download = filename;
+            a.click();
+        });
 }
 
 
