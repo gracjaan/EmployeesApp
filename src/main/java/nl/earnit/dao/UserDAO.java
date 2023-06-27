db7d1b01edbbb8643e64efdd8438a0e615d0a4a6
Tom Hansult <tom.hansult@gmail.com>
2023-06-27 09:45:34 +0200

    small changes to notifications in UserDAO

--- src/main/java/nl/earnit/dao/UserDAO.java
+++ src/main/java/nl/earnit/dao/UserDAO.java
@@ -273,8 +273,7 @@ public class UserDAO extends GenericDAO<User> {
 
     public void changeNotificationToSeen(String notification_id) throws SQLException {
         String query = "UPDATE \"notification\"" +
-                "SET \"seen\" = FALSE" +
-                "WHERE \"id\" = ?";
+                "SET \"seen\" = FALSE WHERE id = ?";
         PreparedStatement statement = this.con.prepareStatement(query);
         PostgresJDBCHelper.setUuid(statement, 1, notification_id);
         ResultSet res = statement.executeQuery();
