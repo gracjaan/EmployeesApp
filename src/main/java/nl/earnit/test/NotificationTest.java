1a0e7a174b27f789224beb75f8886d09ffd2ea8b
Tom Hansult <tom.hansult@gmail.com>
2023-06-28 16:12:53 +0200

    NotificationTest update

--- src/main/java/nl/earnit/test/NotificationTest.java
+++ src/main/java/nl/earnit/test/NotificationTest.java
@@ -73,8 +73,8 @@ public class NotificationTest {
             int count = countNotificationsForCompany();
             String insertNotificationsQuery = "INSERT INTO notification (id, user_id, company_id, worked_week_id, date, seen, type)" +
                     "VALUES" +
-                    "('1aebad42-6a8e-4a5f-8a58-225be9b4uyer', " + user_id + ", "+ company_id + "'b6b0d625-b8f5-4fc7-b1c2-31434519690e', NULL, '2023-06-26', TRUE, 'HOURS'),\n" +
-                    "('6f410e92-3b43-498a-8917-9a54ce2fghty', " + user_id + ", " + company_id + "'b6b0d625-b8f5-4fc7-b1c2-31434519690e', '9e46843e-66cc-4290-9396-456536354703', '2023-06-25', FALSE, 'APPROVED');\n";
+                    "('1aebad42-6a8e-4a5f-8a58-225be9b4uyer', '" + user_id + "', '" + company_id + "', 'b6b0d625-b8f5-4fc7-b1c2-31434519690e', NULL, '2023-06-26', TRUE, 'HOURS'), " +
+                    "('6f410e92-3b43-498a-8917-9a54ce2fghty', '" + user_id + "', '" + company_id + "', 'b6b0d625-b8f5-4fc7-b1c2-31434519690e', '9e46843e-66cc-4290-9396-456536354703', '2023-06-25', FALSE, 'APPROVED');";
             String wwId = "b6b0d625-b8f5-4fc7-b1c2-31434519690e";
             String insertWWQuery = "INSERT INTO worked_week (id, contract_id, year, week, status) " +
                     "VALUES ('" + wwId + "', '" + contract_id + "', 2023, 25, 'CONFIRMED')";
@@ -110,11 +110,11 @@ public class NotificationTest {
             System.out.println("Test3 unsuccessful");
         }
         String message = userDAO.convertToDescription("HOURS", "Amazon", "Software engineer", "Kaya kuchta", "25");
-        assertEquals(message,  "You haven't confirmed hours for Amazon in week 25 yet");
+        assertEquals(message,  "You haven't confirmed hours for Software engineer at Amazon for week 25");
         message = userDAO.convertToDescription("APPROVED", "Twitter","Product Owner", "Kaya kuchta", "25");
         assertEquals(message, "Twitter approved your suggested hours for week 25");
         message = userDAO.convertToDescription("CONFLICT", "Blackrock","Scrum Master", "Gabriel Kudow", "25");
-        assertEquals(message, "Blackrock and Gabriel Kudow have a conflict in week 25");
+        assertEquals(message, "Blackrock and Gabriel Kudow have a conflict for week 25");
     }
 
     @Test
