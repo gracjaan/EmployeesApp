4f61a76d9cfffe73cc6493e6238c5f662e361041
gracjaan <gchmielnicki3005@gmail.com>
2023-06-28 09:32:56 +0200

    Adding javadoc to helper documents

--- src/main/java/nl/earnit/helpers/PostgresJDBCHelper.java
+++ src/main/java/nl/earnit/helpers/PostgresJDBCHelper.java
@@ -6,17 +6,44 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+/**
+ * The type Postgres jdbc helper.
+ */
 public class PostgresJDBCHelper {
+    /**
+     * Gets integer.
+     *
+     * @param set         the set
+     * @param columnLabel the column label
+     * @return the integer
+     * @throws SQLException the sql exception
+     */
     public static Integer getInteger(ResultSet set, String columnLabel) throws SQLException {
         int i = set.getInt(columnLabel);
         return set.wasNull() ? null : i;
     }
 
+    /**
+     * Gets boolean.
+     *
+     * @param set         the set
+     * @param columnLabel the column label
+     * @return the boolean
+     * @throws SQLException the sql exception
+     */
     public static Boolean getBoolean(ResultSet set, String columnLabel) throws SQLException {
         boolean i = set.getBoolean(columnLabel);
         return set.wasNull() ? null : i;
     }
 
+    /**
+     * Sets uuid.
+     *
+     * @param statement the statement
+     * @param index     the index
+     * @param value     the value
+     * @throws SQLException the sql exception
+     */
     public static void setUuid(PreparedStatement statement, int index, String value) throws SQLException {
         PGobject toInsert = new PGobject();
         toInsert.setType("uuid");
@@ -24,6 +51,14 @@ public class PostgresJDBCHelper {
         statement.setObject(index, toInsert);
     }
 
+    /**
+     * Sets boolean.
+     *
+     * @param statement the statement
+     * @param index     the index
+     * @param value     the value
+     * @throws SQLException the sql exception
+     */
     public static void setBoolean(PreparedStatement statement, int index, Boolean value) throws SQLException {
         PGobject toInsert = new PGobject();
         toInsert.setType("bool");
